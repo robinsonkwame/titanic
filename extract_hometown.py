38c6e12c1c5028b57bc70c30ef9905b9a216e91b
Kwame Porter Robinson <kporterrobinson@gmail.com>
2017-05-04 00:26:26 -0400

    Update extract_hometown.py

--- extract_hometown.py
+++ extract_hometown.py
@@ -161,7 +161,7 @@ searchCV.fit(titanic.loc[mask, filtered_cols].values, titanic.loc[mask, 'Survive
 
 # something is wrong between fold and the fit(), see
 #In [154]: from sklearn.cross_validation import KFold
-#/Users/kimrobinson/Desktop/Kwame/WKKF/DataResponse/Huilan/python/env/lib/python3.4/site-packages/sklearn/cross_validation.py:44: DeprecationWarning: This module was deprecated in version 0.18 in favor of the model_selection module into which all the refactored classes and functions are moved. Also note that the interface of the new CV iterators are different from that of this module. This module will be removed in 0.20.
+#.../cross_validation.py:44: DeprecationWarning: This module was deprecated in version 0.18 in favor of the model_selection module into which all the refactored classes and functions are moved. Also note that the interface of the new CV iterators are different from that of this module. This module will be removed in 0.20.
 #  "This module will be removed in 0.20.", DeprecationWarning)
 #
 #
